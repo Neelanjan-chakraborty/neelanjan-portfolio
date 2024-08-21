7d77f636c8cfe4a52d7227930044aa2a6bb10099
Neelanjan Chakraborty <neelanjanchakraborty231@gmail.com>
2024-08-22 04:01:39 +0530

    Optimising for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 import netlify from "@astrojs/netlify/functions";
+import sitemap from '@astrojs/sitemap';
+
 
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://neelanjanchakraborty.in',
+  integrations: [sitemap(),tailwind()],
   adapter: netlify(),
   output: 'server'
 });
